fd53ca757cc3ce2358588486c3f43a16ff0b395e
A.Adefolalu B <adefolalucode101@gmail.com>
2022-08-12 20:53:25 +0100

    write jquery code in jsfile to take information from form in index page

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".form-body").submit(function(event){
+        const input1 = $("value").val().ToUpperCase();
+        const input2 = $("value").val().ToUpperCase();
+        const input3 = $("value").val().ToUpperCase();
+        const input4 = $("value").val().ToUpperCase();
+        const input5 = $("value").val().ToUpperCase();
+        const input6 = $("value").val().ToUpperCase();
+
+        
+
+
+
+        event.preventDefault();
+    });
+
+});
